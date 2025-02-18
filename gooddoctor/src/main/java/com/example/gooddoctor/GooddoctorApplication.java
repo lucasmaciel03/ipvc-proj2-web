ed1f61085d04420d17db4cb8d229d148040c4761
Lucas Maciel <03lucasmaciel@gmail.com>
2025-02-18 09:15:57 +0000

    chore : setup project

---
+++ gooddoctor/src/main/java/com/example/gooddoctor/GooddoctorApplication.java
@@ -0,0 +1,13 @@
+package com.example.gooddoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooddoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooddoctorApplication.class, args);
+    }
+
+}
---
+++ gooddoctor/src/test/java/com/example/gooddoctor/GooddoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gooddoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooddoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
