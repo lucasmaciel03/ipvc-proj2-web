7a78b2d4870682f59cea722c930668934671db3c
Lucas Maciel <03lucasmaciel@gmail.com>
2025-03-03 15:31:08 +0000

    feat (remove): delete old user and consulta tables; add new user, role, and user_roles tables

---
+++ database/schemas/auth/tables/tbl_users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tbl_users(
+    id SERIAL PRIMARY KEY,
+    user_number VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    nif VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
